0571e14882d4f1f97e4642d02db7469bda6f2b4e
Michael JasonSmith <mpj17@onlinegroups.net>
2008-10-29 22:31:02 +0000

    A tweek to the Check Required Fields JavaScript code.
    Clean up the View Audit Trail code, so it is easier to read, and "createObject"
    is called (as God intended).
    
    Change the Set Password code so auditing actually works (the userInfo was not
    being recorded before) and the logged in user is accessed using sane and
    sensible means.
    
    Fix up the event-ID generator so there is more food for the MD5 hash.
    
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/branches/GSAuditTrail/trunk%404478

--- utils.py
+++ utils.py
@@ -1,16 +1,15 @@
 # coding=utf-8
-from datetime import date
+from pytz import UTC
+from datetime import datetime
 from md5 import new as new_md5
 from Products.XWFCore.XWFUtils import convert_int2b62
 
 def event_id_from_data(userInfo, instanceUserInfo, siteInfo, code,
                        instanceDatum, supplementaryDatum):
-    d = date.today()
-    
     e = '%s-%s %s-%s %s-%s %s %s %s %s' % \
       (userInfo.name, userInfo.id, instanceUserInfo.name, 
-       instanceUserInfo.id, siteInfo.name, siteInfo.id, d, code, 
-       instanceDatum, supplementaryDatum)
+       instanceUserInfo.id, siteInfo.name, siteInfo.id, 
+       datetime.now(UTC), code, instanceDatum, supplementaryDatum)
     eNum = long(new_md5(e).hexdigest(), 16)
     eventId = str(convert_int2b62(eNum))
     assert type(eventId) == str
