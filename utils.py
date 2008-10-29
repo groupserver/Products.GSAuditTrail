b9445ae0025a50d536548a2c2834e7c47b889db3
Michael JasonSmith <mpj17@onlinegroups.net>
2008-10-29 23:26:55 +0000

    I hate ASCII. Seems to work with Unicode properly now.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/branches/GSAuditTrail/trunk%404480

--- utils.py
+++ utils.py
@@ -7,8 +7,9 @@ from Products.XWFCore.XWFUtils import convert_int2b62
 def event_id_from_data(userInfo, instanceUserInfo, siteInfo, code,
                        instanceDatum, supplementaryDatum):
     e = '%s-%s %s-%s %s-%s %s %s %s %s' % \
-      (userInfo.name, userInfo.id, instanceUserInfo.name, 
-       instanceUserInfo.id, siteInfo.name, siteInfo.id, 
+      (userInfo.name.encode('utf-8'), userInfo.id, 
+       instanceUserInfo.name.encode('utf-8'), instanceUserInfo.id,
+       siteInfo.name.encode('utf-8'), siteInfo.id, 
        datetime.now(UTC), code, instanceDatum, supplementaryDatum)
     eNum = long(new_md5(e).hexdigest(), 16)
     eventId = str(convert_int2b62(eNum))
