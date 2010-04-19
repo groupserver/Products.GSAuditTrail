1e5a8b3e2a10ce7ec2a0486e60ef1aece887a73b
Michael JasonSmith <mpj17@onlinegroups.net>
2010-04-19 15:50:14 +1000

    Better handling of Unicode in the logging.

--- Products/GSAuditTrail/utils.py
+++ Products/GSAuditTrail/utils.py
@@ -4,13 +4,17 @@ from datetime import datetime
 from md5 import new as new_md5
 from Products.XWFCore.XWFUtils import convert_int2b62
 
+ascii = 'ascii'
+ignore = 'ignore'
+
 def event_id_from_data(userInfo, instanceUserInfo, siteInfo, code,
                        instanceDatum, supplementaryDatum):
     e = '%s-%s %s-%s %s-%s %s %s %s %s' % \
-      (userInfo.name.encode('utf-8'), userInfo.id, 
-       instanceUserInfo.name.encode('utf-8'), instanceUserInfo.id,
-       siteInfo.name.encode('utf-8'), siteInfo.id, 
-       datetime.now(UTC), code, instanceDatum, supplementaryDatum)
+      (userInfo.name.encode(ascii, ignore), userInfo.id, 
+       instanceUserInfo.name.encode(ascii, ignore), instanceUserInfo.id,
+       siteInfo.name.encode(ascii, ignore), siteInfo.id, 
+       datetime.now(UTC), code, instanceDatum.encode(ascii, ignore), 
+       supplementaryDatum.encode(ascii, ignore))
     eNum = long(new_md5(e).hexdigest(), 16)
     eventId = str(convert_int2b62(eNum))
     assert type(eventId) == str
