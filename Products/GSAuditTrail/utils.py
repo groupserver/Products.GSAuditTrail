688d36832ca6c51b3ee5d81e54524e7005256c27
Alice Murphy <alice.murphy@onlinegroups.net>
2010-10-05 23:23:08 +0200

    Turn audit trail db rows into event objects

--- Products/GSAuditTrail/utils.py
+++ Products/GSAuditTrail/utils.py
@@ -2,6 +2,7 @@
 from pytz import UTC
 from datetime import datetime
 from md5 import new as new_md5
+from zope.component import createObject
 from Products.XWFCore.XWFUtils import convert_int2b62
 
 ascii = 'ascii'
@@ -19,4 +20,26 @@ def event_id_from_data(userInfo, instanceUserInfo, siteInfo, code,
     eventId = str(convert_int2b62(eNum))
     assert type(eventId) == str
     return eventId
+
+def marshal_data(context, data):
+    assert context
+    assert type(data) == dict
+    retval = data
+    
+    uId = retval.pop('instance_user_id')
+    retval['instanceUserInfo'] = \
+      createObject('groupserver.UserFromId', context, uId)
+
+    retval.pop('site_id')
+    retval['siteInfo'] = \
+      createObject('groupserver.SiteInfo', context)
+
+    uId = retval.pop('user_id')
+    retval['userInfo'] = \
+      createObject('groupserver.UserFromId', context, uId)
+
+    gId = retval.pop('group_id')
+    retval['groupInfo'] = \
+      createObject('groupserver.GroupInfo', context, gId)
+    return retval
 
